1615b04e6c2a36a5a5ade9d955af32c2ce5578ee
Vartan Shirinyan <02vartan-s@mail.ru>
2024-09-20 22:12:34 +0300

    SOLID

--- HomeWork5/src/model/Type.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-public enum Type {
-    STUDENT,
-    TEACHER
-}
--- HomeWork5/src/view/view/StudentView.java
+++
@@ -1,9 +0,0 @@
-package view;
-
-import model.Student;
-
-public class StudentView {
-    public void printOnConsole(Student student) {
-        System.out.println(student.toString());
-    }
-}
